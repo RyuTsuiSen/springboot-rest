44c357aa5cca5d018741a483d460533daf28fa9b
RyuTsuiSen <banjo8560@hotmail.com>
2018-10-10 21:12:24 +0200

    add async

--- src/main/java/fr/trandutrieu/remy/springbootrest/application/resource/Hello.java
+++ src/main/java/fr/trandutrieu/remy/springbootrest/application/resource/Hello.java
@@ -1,11 +1,17 @@
 package fr.trandutrieu.remy.springbootrest.application.resource;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 import org.springframework.stereotype.Component;
 
+import fr.trandutrieu.remy.socle.audit.Audit;
+import fr.trandutrieu.remy.socle.audit.Audit.Level;
 import fr.trandutrieu.remy.socle.exceptions.BusinessException;
 import fr.trandutrieu.remy.socle.exceptions.BusinessException.BusinessExceptionBuilder;
 import fr.trandutrieu.remy.socle.externalcall.AdapterCall.TYPE_APPEL;
@@ -48,4 +54,33 @@ public class Hello implements Resource{
     	reponse.setReponse("Hello, Welcome to CXF Spring boot " + TYPE_APPEL.ERREUR_DEV + "!!!");
 		return reponse;
 	}
+	
+	@GET
+	@Path("asyncFaux")
+    public void getAsyncFaux(@Suspended final AsyncResponse asyncResponse) throws InterruptedException {
+		Audit.trace(Level.INFO, "ASYNC", "MAIN - Debut getAsyncFaux");
+		TimeUnit.SECONDS.sleep(10);
+		Audit.trace(Level.INFO, "ASYNC", "MAIN - Fin getAsyncFaux");
+		asyncResponse.resume("Fin en asynchrone faux");
+    }
+	
+	@GET
+	@Path("asyncReel")
+    public void getAsyncReel(@Suspended final AsyncResponse asyncResponse) {
+		Audit.trace(Level.INFO, "ASYNC", "MAIN - Debut getAsyncReel");
+		new Thread(new Runnable() {
+            @Override
+            public void run() {
+        		Audit.trace(Level.INFO, "ASYNC", "THREAD - Debut en asynchrone vrai");
+        		try {
+					TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+        		Audit.trace(Level.INFO, "ASYNC", "THREAD - Fin en asynchrone vrai");
+        		asyncResponse.resume("Fin en asynchrone vrai");
+            }
+        }).start();
+		Audit.trace(Level.INFO, "ASYNC", "MAIN - Fin getAsyncReel");
+    }
 }
